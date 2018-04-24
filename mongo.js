3b170e478f726458d55dad8b9eaee17f37508803
Marcelo Rocha DaSilva <marcelorocha8@gmail.com>
2018-04-24 15:52:31 -0400

    fixing database name on mongo.js

--- mongo.js
+++ mongo.js
@@ -1,6 +1,6 @@
 const MongoClient=require('mongodb').MongoClient
 
-const url = 'mongodb://mongodba:data2012@localhost:27010,localhost:27011,localhost:27012/test?replicaSet=replKey';
+const url = 'mongodb://mongodba:data2012@localhost:27010,localhost:27011,localhost:27012/admin?replicaSet=replKey';
 
 MongoClient.connect(url, function(err,client){ 
 	if (err) 
