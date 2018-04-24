d7f51490f90514579f2b66ba6cb38bb850f0acbd
Marcelo Rocha DaSilva <marcelorocha8@gmail.com>
2018-04-24 15:36:55 -0400

    bindip change

---
+++ key-bindip-replset-mongod.sh
@@ -0,0 +1,14 @@
+echo "Clean Directories"
+./cleandir.sh
+echo "Starting mongod daemons"
+
+# openssl rand -base64 755 > mongodb-keyfile
+
+mongod --auth  --keyFile ./mongodb-key-file  --bind_ip 0.0.0.0   --replSet replKey --fork --dbpath /tmp/mongod/10/data/ --logpath /tmp/mongod/10/log/mongod10_log.txt --port 27010 
+mongod --auth  --keyFile ./mongodb-key-file  --bind_ip 0.0.0.0  --replSet replKey --fork --dbpath /tmp/mongod/11/data/ --logpath /tmp/mongod/11/log/mongod11_log.txt --port 27011
+mongod --auth  --keyFile ./mongodb-key-file  --bind_ip 0.0.0.0  --replSet replKey --fork --dbpath /tmp/mongod/12/data/ --logpath /tmp/mongod/12/log/mongod12_log.txt --port 27012 
+echo "Before waiting 10 seconds"
+#sleep 10
+echo "Start replSet"
+# mongo  localhost:27010/admin --shell ./key-rep.js
+echo "Execution finished"
